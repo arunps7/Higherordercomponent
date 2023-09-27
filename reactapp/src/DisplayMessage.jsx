19097fe91f7876aee476513f455d3bcf8a8817c0
arunps7 <arunps4462@gmail.com>
2023-09-27 13:32:43 +0530

    first commit

---
+++ reactapp/src/DisplayMessage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function DisplayMessage({ logMessage }) {
+  return (
+    <div>
+      <h2>Component with Logger</h2>
+      <p>Message from HOC: {logMessage}</p>
+    </div>
+  );
+}
+
+export default DisplayMessage;
